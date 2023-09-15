337a50fcd898c693ab5fb66fd01f71ca5d52d85e
Ceech Hsu <ceechhsu@gmail.com>
2023-09-14 20:40:32 -0700

    Complete FunctionalTrafficLight.tsx

--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -1,17 +1,32 @@
-export const FunctionalTrafficLight = () => {
+import React, { useState } from "react";
+
+export const FunctionalTrafficLight: React.FC = () => {
+  const [trafficLightIndex, setTrafficLightIndex] = useState<number>(0);
+
+  const isRed: string = trafficLightIndex === 0 ? "red" : "black";
+  const isYellow: string = trafficLightIndex === 1 ? "yellow" : "black";
+  const isGreen: string = trafficLightIndex === 2 ? "green" : "black";
+
+  const nextState = () => {
+    if (trafficLightIndex < 2) {
+      setTrafficLightIndex(trafficLightIndex + 1);
+    } else {
+      setTrafficLightIndex(0);
+    }
+  };
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
         {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div className={`circle ${isRed}`}></div>
+        <div className={`circle ${isYellow}`}></div>
+        <div className={`circle ${isGreen}`}></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button className="next-state-button" onClick={nextState}>
+        Next State
+      </button>
     </div>
   );
 };
-
-
-
